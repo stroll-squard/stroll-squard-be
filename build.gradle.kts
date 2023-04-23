0718890096e43b1ffa67355d2dd692de127c2c11
yongckim <zxcv9203@naver.com>
2023-04-23 20:18:14 +0900

    :rocket: CI/CD : CI 도중에 ktlint로 문법 검사하도록 설정

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,8 @@ plugins {
     id("org.springframework.boot") version "3.0.5"
     id("io.spring.dependency-management") version "1.1.0"
     id("org.asciidoctor.jvm.convert") version "3.3.2"
+    id("org.jlleitschuh.gradle.ktlint") version "10.2.0"
+
     kotlin("jvm") version "1.7.22"
     kotlin("plugin.spring") version "1.7.22"
     kotlin("plugin.jpa") version "1.7.22"
@@ -73,4 +75,3 @@ tasks {
         }
     }
 }
-
